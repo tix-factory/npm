ea00a9fab96a779967b4dcd314c1e4130dc835b2
WebGL3D <OgreLandBob@gmail.com>
2020-11-22 15:58:16 -0800

    Add ConfiguredConnection

---
+++ mysql-data/configuredConnection.js
@@ -0,0 +1,59 @@
+import ConnectionPool from "./connectionPool.js";
+
+export default class {
+	constructor(configurationClient, logger, connectionStringSettingName, sslCertificateFileName) {
+		this.connection = null;
+		this.connectionError = null;
+		this.configurationClient = configurationClient;
+		this.logger = logger;
+		this.connectionStringSettingName = connectionStringSettingName;
+		this.sslCertificateFileName = sslCertificateFileName;
+		this.connectionQueue = [];
+		this.initConnection();
+	}
+
+	getConnection() {
+		if (this.connection) {
+			return Promise.resolve(this.connection);
+		} else if (this.connectionError) {
+			return Promise.reject(this.connectionError);
+		} else {
+			return new Promise((resolve, reject) => {
+				this.connectionQueue.push({
+					resolve: resolve,
+					reject: reject
+				});
+			});
+		}
+	}
+
+	initConnection() {
+		this.configurationClient.getSettingValue(this.connectionStringSettingName).then(connectionString => {
+			this.connection = new ConnectionPool(connectionString, {
+				sslCertificateFileName: this.sslCertificateFileName
+			});
+
+			this.connectionQueue.forEach(p => {
+				try {
+					p.resolve(this.connection);
+				} catch (e) {
+					this.logger.warn(e);
+				}
+			});
+
+			this.connectionQueue = null;
+		}).catch(err => {
+			this.connectionError = err;
+
+			this.connectionQueue.forEach(p => {
+				try {
+					p.reject(err);
+				} catch (e) {
+					this.logger.warn(e);
+				}
+			});
+
+			this.connectionQueue = null;
+		});
+	}
+}
--- mysql-data/index.js
+++ mysql-data/index.js
@@ -1,11 +1,13 @@
 import mysql from "mysql";
-import Connection from "./connection.js";
+import ConnectionPool from "./connectionPool.js";
+import ConfiguredConnection from "./configuredConnection.js";
 import mySqlErrors from "./mySqlErrors.js";
 
 export default {
 	mysql_module: mysql,
 
 	errors: mySqlErrors,
-	
-	connection: Connection
+
+	connectionPool: ConnectionPool,
+	ConfiguredConnection: ConfiguredConnection
 };
