2e88cae32e11631b83b7bd6ea8a0ddafc5a80e4d
WebGL3D <OgreLandBob@gmail.com>
2020-11-22 10:20:09 -0800

    Remove console.log(storedProcedureParameters);

--- mysql-data/connection.js
+++ mysql-data/connection.js
@@ -153,8 +153,6 @@ export default class {
 					parameterValuesArray.push(parameterValues[parameterName]);
 				}
 
-				console.log(storedProcedureParameters);
-
 				resolve(parameterValuesArray);
 			}).catch(reject);
 		});
