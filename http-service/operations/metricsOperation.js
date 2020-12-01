4f755466eb001637b90431f35b6c8635a582eb89
Bob Rose <WebGL3D@users.noreply.github.com>
2020-11-30 23:17:32 -0800

    Add metrics endpoint to http-service (#21)

---
+++ http-service/operations/metricsOperation.js
@@ -0,0 +1,38 @@
+import http from "@tix-factory/http";
+
+export default class {
+	constructor(prometheusRegister) {
+		this.prometheusRegister = prometheusRegister;
+	}
+
+	get allowAnonymous() {
+		return false;
+	}
+
+	get name() {
+		return "Metrics";
+	}
+
+	get route() {
+		return "/metrics";
+	}
+
+	get method() {
+		return http.methods.get;
+	}
+
+	get contentType() {
+		return this.prometheusRegister.contentType;
+	}
+
+	get rawResult() {
+		return true;
+	}
+
+	execute() {
+		const metricsBuffer = Buffer.from(this.prometheusRegister.metrics());
+		this.prometheusRegister.resetMetrics();
+
+		return Promise.resolve(metricsBuffer);
+	}
+};
