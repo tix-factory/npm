76f92e2822585a080ca49ad07a0832f9f958ee90
WebGL3D <OgreLandBob@gmail.com>
2020-12-13 08:48:03 -0800

    Include error code in HttpResponseError message

--- http/errors/httpResponseError.js
+++ http/errors/httpResponseError.js
@@ -2,32 +2,45 @@ import HttpRequestError from "./httpRequestError.js";
 const jsonResponseType = "application/json";
 const defaultCode = "unknown";
 
-export default class extends HttpRequestError {
-	constructor(httpRequest, httpResponse, errorMap) {
-		super(httpRequest, httpResponse);
+const getErrorCode = (httpResponse, errorMap) => {
+	let code = defaultCode;
 
-		this.code = defaultCode;
-
-		if (errorMap && httpResponse.statusCode) {
-			const contentType = httpResponse.getHeader("Content-Type");
-			if (contentType.includes(jsonResponseType)) {
-				try {
-					const responseBody = JSON.parse(httpResponse.body.toString());
-					if (typeof(responseBody.error) === "string") {
-						if (errorMap.hasOwnProperty(responseBody.error)) {
-							this.code = errorMap[responseBody.error];
-						} else {
-							for (let key in errorMap) {
-								if (key.toLowerCase() === responseBody.error.toLowerCase()) {
-									this.code = errorMap[key];
-								}
+	if (errorMap && httpResponse.statusCode) {
+		const contentType = httpResponse.getHeader("Content-Type");
+		if (contentType.includes(jsonResponseType)) {
+			try {
+				const responseBody = JSON.parse(httpResponse.body.toString());
+				if (typeof(responseBody.error) === "string") {
+					if (errorMap.hasOwnProperty(responseBody.error)) {
+						code = errorMap[responseBody.error];
+					} else {
+						for (let key in errorMap) {
+							if (key.toLowerCase() === responseBody.error.toLowerCase()) {
+								code = errorMap[key];
 							}
 						}
 					}
-				} catch {
-					// Couldn't parse error code
 				}
+			} catch {
+				// Couldn't parse error code
 			}
 		}
+	}
+
+	return code;
+};
+
+const getMessage = errorCode => {
+	if (!errorCode || errorCode == defaultCode) {
+		return "";
+	}
+
+	return errorCode;
+};
+
+export default class extends HttpRequestError {
+	constructor(httpRequest, httpResponse, errorMap) {
+		super(httpRequest, httpResponse, getMessage(getErrorCode(httpResponse, errorMap)));
+		this.code = getErrorCode(httpResponse, errorMap);
 	}
 }
