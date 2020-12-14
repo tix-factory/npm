4b99694707a0a921fee4503e31aea78d471d0643
WebGL3D <OgreLandBob@gmail.com>
2020-12-13 22:00:54 -0800

    gzip request body

--- http/handlers/compressionHandler.js
+++ http/handlers/compressionHandler.js
@@ -2,6 +2,7 @@ import zlib from "zlib";
 import HttpHandler from "./httpHandler.js";
 import HttpClientError from "../errors/httpClientError.js";
 import httpErrors from "../constants/httpErrors.js";
+const minEncodedRequestBodyLength = 256;
 
 const gunzip = (encodedBuffer) => {
 	return new Promise((resolve, reject) => {
@@ -15,6 +16,18 @@ const gunzip = (encodedBuffer) => {
 	});
 };
 
+const gzip = (buffer) => {
+	return new Promise((resolve, reject) => {
+		zlib.gzip(buffer, (err, encodedBuffer) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(encodedBuffer);
+			}
+		});
+	});
+};
+
 export default class extends HttpHandler {
 	constructor() {
 		super();
@@ -23,11 +36,20 @@ export default class extends HttpHandler {
 	async execute(httpRequest) {
 		httpRequest.addOrUpdateHeader("Accept-Encoding", "gzip");
 
+		if (httpRequest.body && httpRequest.body.length >= minEncodedRequestBodyLength) {
+			const requestContentEncoding = httpRequest.getHeader("content-encoding");
+			switch (requestContentEncoding) {
+				case "gzip":
+					httpRequest.body = await gzip(httpRequest.body);
+					break;
+			}
+		}
+
 		const httpResponse = await super.execute(httpRequest);
 
 		if (httpResponse.body.length > 0) {
-			let contentEncoding = httpResponse.getHeader("content-encoding");
-			switch (contentEncoding) {
+			const responseContentEncoding = httpResponse.getHeader("content-encoding");
+			switch (responseContentEncoding) {
 				case "gzip":
 					httpResponse.body = await gunzip(httpResponse.body);
 					break;
