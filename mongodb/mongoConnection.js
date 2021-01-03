1c187c1673691dc32059d9126ac14daea110dccb
Bob Rose <WebGL3D@users.noreply.github.com>
2021-01-02 17:27:20 -0800

    Satisfy mongo warning (#46)

--- mongodb/mongoConnection.js
+++ mongodb/mongoConnection.js
@@ -2,53 +2,36 @@ import MongoDB from "mongodb";
 import Collection from "./collection.js";
 import IdGenerator from "./idGenerator.js";
 
-const connect = (connectionString) => {
-	return new Promise((resolve, reject) => {
-		MongoDB.MongoClient.connect(connectionString, (err, connection) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(connection);
-			}
-		});
-	});
-};
-
 export default class {
 	constructor(connectionString, idGenerator) {
 		this.connectionString = connectionString;
 		this.idGenerator = idGenerator || new IdGenerator();
-		this.connection = null;
+
+		this.client = new MongoDB.MongoClient(connectionString, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		});
+
 		this.databases = {};
 	}
 
-	async connect(projectName) {
-		try {
-			let database = this.databases[projectName.toLowerCase()];
-			if (database) {
-				return Promise.resolve(database);
-			}
-
-			let connection = this.connection;
-			if (!connection) {
-				this.connection = connection = await connect(this.connectionString);
-			}
-
-			this.databases[projectName.toLowerCase()] = database = connection.db(projectName);
-			return Promise.resolve(database);
-		} catch (e) {
-			return Promise.reject(e);
-		}
+	async init() {
+		await this.client.connect();
 	}
 
-	async getCollection(projectName, collectionName, options) {
-		try {
-			const database = await this.connect(projectName);
-			const collection = new Collection(database.collection(collectionName), this.idGenerator, options);
-
-			return Promise.resolve(collection);
-		} catch (e) {
-			return Promise.reject(e);
+	getDatabase(projectName) {
+		let database = this.databases[projectName.toLowerCase()];
+		if (database) {
+			return database;
 		}
+
+		database = this.databases[projectName.toLowerCase()] = this.client.db(projectName);
+		return database;
+	}
+
+	getCollection(projectName, collectionName, options) {
+		const database = this.getDatabase(projectName);
+		const collection = database.collection(collectionName);
+		return new Collection(collection, this.idGenerator, options);
 	}
 };
